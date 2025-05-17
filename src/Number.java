e76a5eaebfc8119f86b0fee820905fa515baf52d
h4unguyxn <h4unguyxn@gmail.com>
2025-05-17 13:27:38 +0700

    Delete src directory

--- src/Number.java
+++
@@ -1,6 +0,0 @@
-public class Number {
-    public static void TestNumber() {
-        int x = 10;
-        System.out.println("Giá trị của x là: " + x);
-    }
-}
--- src/randNum.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-
-public class randNum {
-    public static int randNum() {
-        Random random = new Random();
-        return random.nextInt(100); // Trả về số ngẫu nhiên từ 0-99
-    }
-}
