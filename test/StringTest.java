e29a121ee583b91a34fe6e326b760457eea64eaa
h4unguyxn <h4unguyxn@gmail.com>
2025-05-27 09:59:22 +0700

    add file

--- test/StringTest.java
+++
@@ -1,8 +0,0 @@
-public class StringTest {
-    public static void test() {
-        String s = "Xin chào";
-        System.out.println("Chuỗi: " + s);
-        System.out.println("Viết hoa: " + s.toUpperCase());
-        System.out.println("Chiều dài: " + s.length());
-    }
-}
--- test/TestNumber.java
+++
@@ -1,5 +0,0 @@
-public class TestNumber {
-    public static void main(String[] args) {
-        Number.TestNumber();
-    }
-}
